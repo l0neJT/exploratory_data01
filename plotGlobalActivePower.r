ff7707db51473bf21bcee75d39908aa9d220dc63
l0neJT <loganjtravis@gmail.com>
2014-05-09 19:46:42 -0700

    Create plotGlobalActivePower Function
    Create plotGlobalActivePower function to create a histogram of global
    active power data. Saved output as plotGlobalActivePower.png to local
    directory.

---
+++ plotGlobalActivePower.r
@@ -0,0 +1,31 @@
+## Create a histogram of Global Active Power data
+## Saves as a PNG to directory/file
+
+plotGlobalActivePower <- function(DT = getHousePowerAsDT(),
+                                  file = "Global_Active_Power.png",
+                                  directory = "./") {
+    # Set global plotting parameters
+    # Stores original global plotting parameters reset at function end
+    origBG <- par("bg")
+    par(bg = "transparent")
+    
+    # Set plot parameters
+    labelMain <- "Global Active Power"
+    labelX <- "Global Active Power (kilowats)"
+    color <- "red"
+    
+    # Open PNG plot device to directory/file
+    png(filename = paste(directory, file, sep = ""))
+    
+    # Create plot
+    hist(DT$Global_active_power, main = labelMain, xlab = labelX, col = color)
+    
+    # Close PNG plot device
+    dev.off()
+    
+    # Re-set global plotting parameters
+    par(bg = origBG)
+    
+    # Return character vector of directory/file
+    paste(directory, file, sep = "")
+}
